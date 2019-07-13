fb0dba7cd8e4f3ad3e2cb283ae0eb59fd0a6c42b
andresokol <sokolov-as-i@yandex.ru>
2019-07-13 17:45:46 +0300

    triangle

--- main.cpp
+++ main.cpp
@@ -14,6 +14,8 @@
 
 // std::shared_ptr<BaseRenderer> m_renderer;
 
+GLfloat vertices[] = {-0.5f, -0.5f, 0.0f, 0.5f, -0.5f, 0.0f, 0.0f, 0.5f, 0.0f};
+
 int main(int argc, char *argv[]) {
   glfwInit();
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -45,87 +47,95 @@ int main(int argc, char *argv[]) {
   while (!glfwWindowShouldClose(window)) {
     glfwPollEvents();
 
-    //    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-    //    glClear(GL_COLOR_BUFFER_BIT);
-    //    glMatrixMode(GL_PROJECTION);
-    //    glLoadIdentity();
-    //    gluOrtho2D(0.0, (GLfloat)width, 0.0, (GLfloat)height);
-    //    glMatrixMode(GL_MODELVIEW);
-    //    glLoadIdentity();
-    // Метка в виде крестика
-    //    GLubyte bitmap_mark[] = {0x10, 0x10, 0x10, 0xFF, 0x10, 0x10, 0x10};
-    //
-    //    // Свойства распаковки
-    //    glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
-    //
-    //    // Цвет метки
-    //    glColor3d(1., 0., 0.);
-    //
-    //    // Позиция вывода растра
-    //    glRasterPos2d(25., 25.);
+    // Wireframe
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
-    //    // Вывод битового массива
-    //    glBitmap(7, 7, 4, 3, 0, 0, bitmap_mark);
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
 
-    int W = 10, H = 10;
+    // DRAW START
+    GLuint VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    unsigned char data[W][H][3];
-    for (size_t x = 0; x < H; x++) {
-      for (size_t y = 0; y < W; y++) {
-        data[x][y][0] = 250;
-        data[x][y][1] = 2;
-        data[x][y][2] = 250;
-      }
+    GLuint vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+
+    const auto vertexShaderSource =
+        "#version 330 core\n"
+        "\n"
+        "layout (location = 0) in vec3 position;\n"
+        "\n"
+        "void main()\n"
+        "{\n"
+        "    gl_Position = vec4(position.x, position.y, position.z, 1.0);\n"
+        "}";
+
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    GLint success;
+    GLchar infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+      glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+      std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n"
+                << infoLog << std::endl;
     }
-    //
-    //    glClearColor(0.0, 0.0, 0.0, 0.0);
-    //    glShadeModel(GL_FLAT);
-    //    glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
-    //
-    //    glClear(GL_COLOR_BUFFER_BIT);
-    //    glRasterPos2i(0, 0);
-    //    glDrawPixels(width, height, GL_RGB, GL_UNSIGNED_BYTE, data);
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    glRasterPos2d(-4.5, -3); // нижний левый угол
-    glPixelZoom(1, 1);
-    glPixelStorei(GL_UNPACK_ALIGNMENT, 1); // выравнивание
-    glDrawPixels(W, H,                     // ширина и высота
-                 GL_RGB, GL_UNSIGNED_BYTE, // формат и тип
-                 data);                    // сами данные
+    const auto fragmentShaderSource =
+        "#version 330 core\n"
+        "\n"
+        "out vec4 color;\n"
+        "\n"
+        "void main()\n"
+        "{\n"
+        "\tcolor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+        "}";
+
+    GLuint fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+    GLuint shaderProgram;
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+    glUseProgram(shaderProgram);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat),
+                          (GLvoid *)0);
+    glEnableVertexAttribArray(0);
+
+    GLuint VAO;
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+    // 2. Копируем наш массив вершин в буфер для OpenGL
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    // 3. Устанавливаем указатели на вершинные атрибуты
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat),
+                          (GLvoid *)0);
+    glEnableVertexAttribArray(0);
+    // 4. Отвязываем VAO
+    glBindVertexArray(0);
+
+    // DRAW END
+
+    glUseProgram(shaderProgram);
+    glBindVertexArray(VAO);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glBindVertexArray(0);
 
     glfwSwapBuffers(window);
   }
 
   glfwTerminate();
   return 0;
-
-  //  Settings settings;
-  //  settings.init_from_args(argc, argv);
-  //
-  //  m_renderer = std::make_shared<SamplerRenderer>(
-  //      std::make_shared<Settings>(settings),
-  //      std::make_shared<Scene>(Scene()));
-  //
-  //  m_renderer->init();
-  //  m_renderer->scene->loadGeometry("../geometry_samples/scene");
-  //
-  //  m_renderer->render();
-  //
-  //  //  for (const auto &prim : m_renderer->scene->primitives) {
-  //  //    std::cout << typeid(prim).name() << std::endl;
-  //  //  }
-  //  //
-  //  //  std::cout << typeid(BaseRenderer).name() << std::endl;
-  //
-  //  SDL_Event event;
-  //  while (SDL_WaitEvent(&event) >= 0) {
-  //    if (event.type == SDL_QUIT) {
-  //      break;
-  //    }
-  //  }
-  //
-  //  m_renderer->deinit();
-  //
-  //  return 0;
 }
